f20252adb5fd599f2f52dd8663e3cb7f8d8074aa
Scott Dover <sdover102@gmail.com>
2018-09-10 08:32:20 -0400

    Add meta properties

--- src/components/DetailsView.jsx
+++ src/components/DetailsView.jsx
@@ -2,9 +2,15 @@ import data from '../data';
 import React from 'react';
 import IdentifyPage from './IdentifyPage';
 
+const strip = (html) => {
+   var tmp = document.createElement('div');
+   tmp.innerHTML = html;
+   return (tmp.textContent || tmp.innerText || '').replace(/\r|\n/g, ' ');
+};
+
 export const DetailsView = ({ female }) => (
     <div className="details main-content">
-        <IdentifyPage title={female.name} />
+        <IdentifyPage title={female.name} content={strip(female.content)} />
         <h2>{female.name}</h2>
         <div dangerouslySetInnerHTML={{ __html: female.content }} />
     </div>
--- src/components/IdentifyPage.jsx
+++ src/components/IdentifyPage.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
+const DEFAULT_TITLE = 'Kick-Ass Women';
+const DEFAULT_CONTENT = 'This year, I had the pleasure of learning and writing about amazing, influential, kick-ass women for 26 straight sprints.';
+const MAX_LENGTH = 250;
+
 export class IdentifyPage extends React.Component {
     render() {
         return null;
     }
 
-    componentDidMount() {
-        const { title } = this.props;
+    updateTitleAndMeta() {
+        const { title, content, image } = this.props;
         const pageTitle = title
             ? title + ' | Kick-Ass Women'
-            : 'Kick-Ass Women';
+            : DEFAULT_TITLE;
+        const pageContent = (content || DEFAULT_CONTENT).substr(0, MAX_LENGTH);
 
-        document.getElementsByTagName('title')[0].innerHTML = pageTitle;
+        document.querySelector('title').innerHTML = pageTitle;
+        document.querySelector('[property="og:title"]').setAttribute('content', pageContent);
+        document.querySelector('[property="og:description"]').setAttribute('content', pageContent);
+
+        // Do it
+        // document.querySelector('[property="og:image"]').setAttribute('content', '');
+
+        document.querySelector('[property="og:url"]').setAttribute('content', window.location.href);
+    }
+
+    componentDidUpdate() {
+        this.updateTitleAndMeta();
+    }
+
+    componentDidMount() {
+        this.updateTitleAndMeta();
     }
 }
 
